7d5fa1502df2ecd447e2bdda626ea598717fa06a
Jonathan Riffo <jonathan.riffo7@gmail.com>
2022-06-16 01:15:42 -0300

    Plantilla base sin errores

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="h1">
+		<h1>Adivina el Color<br/><span id="colorDisplay"></span></h1>
+	</div>
+	<div id="stripe">
+		<button id="reset" /*onClick={reset}*/>Nuevos Colores</button>
+		<span id="message"></span>
+		<button id="easy" /*onClick={easy}*/>Facil</button>
+		<button className="selected" id="hard" /*onClick={hard}*/>Dificil</button>
+	</div>
+	<div className="container">
+		<div className="square" id="1"></div>
+		<div className="square" id="2"></div>
+		<div className="square" id="3"></div>
+		<div className="square" id="4"></div>
+		<div className="square" id="5"></div>
+		<div className="square" id="6"></div>
+	</div>
     </div>
   );
 }
